0061870c6be22fbc1df99bde73e53c0084be47d4
Vyacheslav <slavagalochkin.77790@gmail.com>
2023-05-05 19:51:32 +0300

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+    <div className="app-info">
+        <h1>Учет сотрудников в компании N</h1>
+        <h2>Общее число сотрудника: </h2>
+        <h>Премию получат: </h>
+    </div>
+    )
+}
+
+ 
+export default AppInfo;
